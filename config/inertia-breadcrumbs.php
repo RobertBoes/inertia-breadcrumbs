11fd7962bb663ed542983c0e492e409dbfc915b6
Robert Boes <github@robertboes.nl>
2022-01-01 04:52:30 +0100

    Add docs to config

--- config/inertia-breadcrumbs.php
+++ config/inertia-breadcrumbs.php
@@ -2,9 +2,20 @@
 
 return [
     'middleware' => [
+        /**
+         * Determines if the middleware should automatically be registered by this package
+         * If you would like to register it yourself you should set this to false
+         */
         'enabled' => true,
+
+        /**
+         * The middleware is added to the 'web' group by default
+         */
         'group' => 'web',
     ],
 
+    /**
+     * By default a collector for diglactic/laravel-breadcrumbs is used
+     */
     'collector' => RobertBoes\InertiaBreadcrumbs\LaravelBreadcrumbsCollector::class,
 ];
